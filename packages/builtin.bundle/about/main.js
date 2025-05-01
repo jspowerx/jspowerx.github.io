6d7a5937f62783361867278b40775229cab1a334
everettjf <everettjf@live.com>
2025-05-01 09:48:52 -0700

    add built in bundle for demo

---
+++ packages/builtin.bundle/about/main.js
@@ -0,0 +1,10 @@
+
+var onMenuClicked = function(identifier){
+    
+    system.log('about clicked');
+    system.openURL('https://xnu.app/jspower');
+    
+    return {
+        'result':true
+    };
+}
